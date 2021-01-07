788e9f5651ba62f4d9641b9eedc515180fc86ace
Eggnog <75511393+phineapuuu@users.noreply.github.com>
2021-01-07 14:37:35 +0800

    refactor(v2): titles now added again, removed roles from official server

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    "extends": "airbnb-base",
+    "rules": {
+        "default-case": 0,
+        "no-param-reassign": 0,
+        "no-plusplus": 0,
+        "class-methods-use-this": 0,
+        "no-case-declarations": 0,
+        "comma-dangle": 0,
+        "no-undef": 0,
+        "no-console": 0,
+        "no-useless-escape": 0,
+        "consistent-return": 0,
+        "padded-blocks": 0,
+        "no-return-assign": 0,
+        "no-confusing-arrow": 0,
+        "max-len": 0,
+        "no-unused-expressions": 0
+    }
+};
