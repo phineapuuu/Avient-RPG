71b9e3c08cafc3b3baa6600225c70795c5bf7e7a
Eggnog <75511393+phineapuuu@users.noreply.github.com>
2021-01-07 14:33:18 +0800

    add(app.js): now creating logs folder if not exist

---
+++ app.js
@@ -0,0 +1,18 @@
+  
+require('dotenv').config();
+const fs = require('fs');
+const dir = './logs';
+
+if (!fs.existsSync(dir)){
+  console.log('Logs folder created');
+  fs.mkdirSync(dir);
+}
+
+console.log(`${process.env.NODE_ENV.includes('production') ? 'Running Production Env' : 'Running Development Env'}`);
+const DiscordBot = require('./idle-rpg/v2/DiscordBot');
+const { errorLog } = require('./idle-rpg/utils/logger');
+
+process.on('unhandledRejection', (err) => {
+  console.log(err);
+  errorLog.error({ err });
+});
